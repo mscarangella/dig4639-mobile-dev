6e62a384b91ba23ef6e8eccae5488e806b00d960
mscarangella <59657676+mscarangella@users.noreply.github.com>
2020-02-12 12:15:14 -0500

    Tic Tac Toe Challenge #96

--- ice/w6a/src/index.js
+++ ice/w6a/src/index.js
@@ -115,12 +115,12 @@ function calculateWinner(squares) {
     [1, 5, 9, 13],
     [2, 6, 10, 14],
     [3, 7, 11, 15],
-    [0, ],
-    //come back and then when you finish commit #96
+    [0, 5, 10, 15],
+    [3, 6, 9, 11]
   ];
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+    const [a, b, c, d] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]) {
       return squares[a];
     }
   }
