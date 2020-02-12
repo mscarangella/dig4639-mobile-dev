494a909d0848fe469019cbb06376bc24de448660
mscarangella <59657676+mscarangella@users.noreply.github.com>
2020-02-12 11:43:59 -0500

    Updates

---
+++ precache-manifest.b36163d9cb075547633b44c7aef2f3e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db38a3d3a9c398818d21db7b09f6f46",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "1b7c9233e726900ddf80",
+    "url": "/dig4639-mobile-dev/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "0d0ae6ee3ad0d33d12a4",
+    "url": "/dig4639-mobile-dev/static/js/2.a0b43dfa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.a0b43dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b7c9233e726900ddf80",
+    "url": "/dig4639-mobile-dev/static/js/main.515009ca.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
