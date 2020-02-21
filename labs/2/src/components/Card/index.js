92cdbf553c776ae6649f6eddd83504066b551c3a
mscarangella <59657676+mscarangella@users.noreply.github.com>
2020-02-21 16:50:21 -0500

    lab 3 what am i doing

--- labs/2/src/components/Card/index.js
+++ labs/2/src/components/Card/index.js
@@ -1,19 +1,19 @@
-import "./index.css";
+import './index.css'
 
 class Card {
-    constructor(props) {
-        this.props = props;
-        this.element = document.createElement("div");
-        this.element.innerHTML = this.props.content;
-        let span = document.createElement("span");
-        this.element.appendChild(span);
-        span.innerHTML = this.props.content;
-        this.element.className = "card";
-    }
+  constructor (props) {
+    this.props = props
+    this.element = document.createElement('div')
+    this.element.innerHTML = this.props.content
+    const span = document.createElement('span')
+    this.element.appendChild(span)
+    span.innerHTML = this.props.content
+    this.element.className = 'card'
+  }
 
-    render() {
-        return this.element;
-    }
+  render () {
+    return this.element
+  }
 }
 
-export default Card;
+export default Card
