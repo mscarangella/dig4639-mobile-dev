d091f0a70d51dac5badb51b4d2e62aa6cf28a886
mscarangella <59657676+mscarangella@users.noreply.github.com>
2020-03-01 19:29:01 -0500

    Lab 4 Completed

--- labs/4/src/App.js
+++ labs/4/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Card from './components/Card/index.js';
-//import CardList from './components/CardList/index.js';
+import CardList from './components/CardList/index.js';
 
 class App extends React.Component{
   render(){
       return (
-      <Card content="This is a card!"></Card>
+        <div className="App">
+          <CardList />
+        </div>
     )
   }
 };
--- labs/4/src/components/Card/index.js
+++ labs/4/src/components/Card/index.js
@@ -1,10 +1,21 @@
-import './index.css';
-import React from 'react';
+import React from "react";
+import "./index.css";
 
-class Card extends React.Component{
-  render(){
-    return <div class="card"><h1>{this.props.content}</h1></div>;
+class Card extends React.Component {
+
+  render() {
+    return(
+      <div className="card">
+        <span className="close" 
+        onClick={this.props.dataclick} 
+        datatitle={this.props.title}>&times;</span>
+        <h3>{this.props.title}</h3>
+        <p>{this.props.content}</p>
+      </div>
+    );
   }
+
+
 }
 
 export default Card;
