1a6861591038aa6155fe306eb0eb453b042f40d6
mscarangella <59657676+mscarangella@users.noreply.github.com>
2020-03-24 12:09:06 -0400

    zooom

--- demo/w12a/server/index.js
+++ demo/w12a/server/index.js
@@ -1,17 +1,22 @@
 // server/index.js
 // https://expressjs.com/en/starter/hello-world.html
 const express = require('express')
-const fetch = require("node-fetch");
+// const fetch = require('node-fetch')
 const cors = require('cors')
 const app = express()
 const port = 3001
-const simpleJson = {a: 10, 
-  b: "Hello DIG4639!", 
-  list:[
-    {title:"Tonight",
-    content: "Watching Netflix"},
-    {title:"Tomorrow",
-    content: "Completing Homework"}
+const simpleJson = {
+  a: 10,
+  b: 'Hello DIG4639!',
+  list: [
+    {
+      title: 'Tonight',
+      content: 'Watching Netflix'
+    },
+    {
+      title: 'Tomorrow',
+      content: 'Completing Homework'
+    }
   ]
 }
 app.use(cors())
