a045fa501d2163a04c5a54302f2b4ba149bda7ea
mscarangella <59657676+mscarangella@users.noreply.github.com>
2020-03-31 16:26:48 -0400

    progress 5

--- labs/5/src/App.js
+++ labs/5/src/App.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import './App.css';
-import CardList from './components/CardList';
+import Card from './components/Card';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <CardList />
-      </div>
-    );
-  }
-}
+  class App extends React.Component{
+    constructor(props){
+      super(props)
+      this.state = {content:"", list: []}
+    }
+    async getServerData(){
+      const response = await fetch("https://api.weather.gov/gridpoints/MLB/25,69/forecast")
+      console.log("Here's the weather!")
+      console.log(response)
+      let obj = await response.json()
+      console.log("Processed weather as JSON: ", obj)
+      this.setState({content:obj.b})
+      this.setState({list:obj.list})
+    }
+    componentDidMount(){
+      console.log("Component did mount!")
+      this.getServerData()
+    }
+    render(){
+      return(
+        <div>
+          <p>{this.state.content}</p>
+          {this.state.list.map((listObject, index) =>
+          <Card key={index} title={listObject.title}>{listObject.content}</Card>)}
+        </div>
+      )
+    }
+    }
 
-export default App;
+    export default App;
--- labs/5/src/components/Card/index.js
+++ labs/5/src/components/Card/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './index.css';
 
-class Card extends React.Component {
-
-  render() {
+class Card extends React.Component{
+  render(){
     return(
-      <div className="card">
-        <span className="close" 
-        onClick={this.props.dataclick} 
-        datatitle={this.props.title}>&times;</span>
-        <h3>{this.props.title}</h3>
-        <p>{this.props.content}</p>
+      <div className='card'>
+        <h2>{this.props.title}</h2>
+        {this.props.children}
       </div>
-    );
+    )
   }
-
-
 }
 
 export default Card;
